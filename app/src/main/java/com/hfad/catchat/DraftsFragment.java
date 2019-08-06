7013e50088c2f94b6adc94ff0e32625685aeb25a
lgsd123456 <hbhg8612814@hotmail.com>
2019-08-06 17:59:15 +0800

    创建滑动抽屉的布局

---
+++ app/src/main/java/com/hfad/catchat/DraftsFragment.java
@@ -0,0 +1,30 @@
+package com.hfad.catchat;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.hfad.beeradviser.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DraftsFragment extends Fragment {
+
+
+    public DraftsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_drafts, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/hfad/catchat/Main3Activity.java
@@ -0,0 +1,15 @@
+package com.hfad.catchat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hfad.beeradviser.R;
+
+public class Main3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+}
